a65efc9a0d637b06c32b82054b97157f802c4ace
Mayeul Deries <mayeul.deries@gmail.com>
2025-03-23 12:27:00 +0100

    add connection to database

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import cors from 'cors';
 import express from 'express';
+import connectDB from './src/database/connectToDB.js';
 import 'dotenv/config';
 
 const port = process.env.PORT;
@@ -16,7 +17,6 @@ const app = express();
 app.use(express.json());
 app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
-
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
@@ -25,6 +25,8 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
   res.send('Hello from the server!');
 });
+
+connectDB();
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port} 🚀`);
