0674a772806dc03fe695a7d0022ad667adfc99e5
Mayeul Deries <mayeul.deries@gmail.com>
2025-03-23 12:12:50 +0100

    feat: configure server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,31 @@
+import cors from 'cors';
 import express from 'express';
+import 'dotenv/config';
+
+const port = process.env.PORT;
+
+const corsOptions = {
+  origin: 'http://localhost:5173/',
+  credentials: true,
+  methods: 'GET, POST, PUT, DELETE',
+  preflightContinue: true,
+};
 
 const app = express();
-const port = 3000;
+
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello from the server!');
 });
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port} 🚀`);
 });
