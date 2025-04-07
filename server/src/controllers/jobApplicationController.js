6fe4d4d2eeb9c3611ec332f21f45d60afc94769c
Mayeul Deries <mayeul.deries@gmail.com>
2025-04-07 21:20:54 +0200

    feat: add jobApplicationController with verifyToken

--- server/src/controllers/jobApplicationController.js
+++ server/src/controllers/jobApplicationController.js
@@ -1,8 +1,12 @@
 import jobApplicationModel from '../models/jobApplicationModel.js';
 
 export const getJobApplications = async (req, res) => {
+  const { userId } = req;
   try {
-    const jobApplications = await jobApplicationModel.find({});
+    const jobApplications = await jobApplicationModel.find({ userId });
+    if (!jobApplications) {
+      return res.status(404).json({ message: 'No job applications found' });
+    }
     res.status(200).json({ message: 'jobApplications successfully recovered', jobApplications });
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -10,12 +14,15 @@ export const getJobApplications = async (req, res) => {
 };
 
 export const createJobApplication = async (req, res) => {
-  const { title, company, link, date, status, notes, categoryId } = req.body;
+  const { userId } = req;
+
+  const { title, company, link, date, status, notes, category } = req.body;
   try {
-    if (!title || !company || !date || !status || !categoryId) {
+    if (!title || !company || !date || !status || !category) {
       return res.status(400).json({ message: 'Missing required fields' });
     }
-    const jobApplication = await jobApplicationModel.create(req.body);
+    const jobApplication = await jobApplicationModel.create({ ...req.body, userId });
+
     res.status(201).json({ message: 'jobApplication successfully created', jobApplication });
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -24,7 +31,10 @@ export const createJobApplication = async (req, res) => {
 
 export const getJobApplication = async (req, res) => {
   try {
-    const jobApplication = await jobApplicationModel.findById(req.params.id);
+    const jobApplication = await jobApplicationModel.findOne({ _id: req.params.id });
+    if (!jobApplication) {
+      return res.status(404).json({ message: 'jobApplication not found' });
+    }
     res.status(200).json({ message: 'jobApplication successfully recovered', jobApplication });
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -32,8 +42,21 @@ export const getJobApplication = async (req, res) => {
 };
 
 export const updateJobApplication = async (req, res) => {
+  const { title, company, link, date, status, notes, category } = req.body;
   try {
-    const jobApplication = await jobApplicationModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!title || !company || !date || !status || !category) {
+      return res.status(400).json({ message: 'Missing required fields' });
+    }
+    const jobApplication = await jobApplicationModel.findOneAndUpdate(
+      { _id: req.params.id },
+      { ...req.body },
+      { new: true }
+    );
+
+    if (!jobApplication) {
+      return res.status(404).json({ message: 'jobApplication not found' });
+    }
+
     res.status(200).json({ message: 'jobApplication successfully updated', jobApplication });
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -42,8 +65,11 @@ export const updateJobApplication = async (req, res) => {
 
 export const deleteJobApplication = async (req, res) => {
   try {
-    await jobApplicationModel.findByIdAndDelete(req.params.id);
-    res.status(200).json({ message: 'jobApplication successfully deleted' });
+    const jobApplication = await jobApplicationModel.findOneAndDelete({ _id: req.params.id });
+    if (!jobApplication) {
+      return res.status(404).json({ message: 'jobApplication not found' });
+    }
+    res.status(200).json({ message: 'jobApplication successfully deleted', jobApplication });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
