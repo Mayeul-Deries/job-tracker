f11e42b76eef07d6ccb68f51a7674882ed75f5b0
Mayeul Deries <mayeul.deries@gmail.com>
2025-04-09 10:20:47 +0200

    feat: add userController

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -2,7 +2,7 @@ import userModel from '../models/userModel.js';
 
 export const getUser = async (req, res) => {
   try {
-    const user = await userModel.findById(req.params.id);
+    const user = await userModel.findOne({ _id: req.params.id });
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
@@ -12,6 +12,28 @@ export const getUser = async (req, res) => {
   }
 };
 
-export const updateUser = async (req, res) => {};
+export const updateUser = async (req, res) => {
+  try {
+    const user = await userModel.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).json({ message: 'User successfully updated', user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-export const deleteUser = async (req, res) => {};
+export const deleteUser = async (req, res) => {
+  try {
+    const user = await userModel.findOneAndDelete({ _id: req.params.id });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.clearCookie('__jt_token');
+
+    res.status(200).json({ message: 'User successfully deleted' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
