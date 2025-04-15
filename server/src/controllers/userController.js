d20fdd8576ed2e4355aa792e3288c82d792af8f1
Mayeul Deries <mayeul.deries@gmail.com>
2025-04-15 18:15:17 +0200

    feat: check unicity of email and username in updateUser

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -14,6 +14,32 @@ export const getUser = async (req, res) => {
 
 export const updateUser = async (req, res) => {
   try {
+    if (req.body.email) {
+      const existingEmail = await userModel.findOne({
+        email: req.body.email.toLowerCase(),
+        _id: { $ne: req.body.id },
+      });
+
+      if (existingEmail) {
+        return res.status(409).json({ message: 'Email already exists' });
+      }
+
+      req.body.email = req.body.email.toLowerCase();
+    }
+
+    if (req.body.username) {
+      const existingUsername = await userModel.findOne({
+        username: req.body.username.toLowerCase(),
+        _id: { $ne: req.body.id },
+      });
+
+      if (existingUsername) {
+        return res.status(409).json({ message: 'Username already exists' });
+      }
+
+      req.body.username = req.body.username.toLowerCase();
+    }
+
     const user = await userModel.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true });
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
