a2adb2d0fcde81157492362dbaf4c1187409730d
Mayeul Deries <mayeul.deries@gmail.com>
2025-05-22 14:27:05 +0200

    fix cors request error

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,7 @@ import 'dotenv/config';
 const port = process.env.PORT;
 
 const corsOptions = {
-  origin: 'http://localhost:5173/',
+  origin: 'http://localhost:5173',
   credentials: true,
   methods: 'GET, POST, PUT, DELETE',
   preflightContinue: true,
