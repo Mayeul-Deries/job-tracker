da42dbbb30d5caab362e79da752e49aeb16a1024
Mayeul Deries <mayeul.deries@gmail.com>
2025-06-04 20:38:39 +0200

    add create jobApplication form

--- client/src/components/customs/StatusSelect.tsx
+++ client/src/components/customs/StatusSelect.tsx
@@ -1,42 +1,37 @@
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Badge } from '@/components/ui/badge';
-import { StatusOffer } from '@/constants/statusOffer';
 import { cn } from '@/lib/utils';
 import { useTranslation } from 'react-i18next';
+import { StatusOffer } from '@/constants/statusOffer';
 
-export const StatusSelect = ({ value, onChange }: { value: keyof typeof statusLabels; onChange: (value: keyof typeof statusLabels) => void }) => {
-  const statusColors: Record<string, string> = {
-    [StatusOffer.SENT]: 'bg-blue-100 text-blue-700',
-    [StatusOffer.FOLLOWED_UP]: 'bg-yellow-100 text-yellow-700',
-    [StatusOffer.INTERVIEW_SCHEDULED]: 'bg-purple-100 text-purple-700',
-    [StatusOffer.ACCEPTED]: 'bg-green-100 text-green-700',
-    [StatusOffer.REJECTED]: 'bg-red-100 text-red-700',
-  };
+const statusOptions = [
+  { value: StatusOffer.SENT, color: 'bg-blue-100 text-blue-700' },
+  { value: StatusOffer.FOLLOWED_UP, color: 'bg-yellow-100 text-yellow-700' },
+  { value: StatusOffer.INTERVIEW_SCHEDULED, color: 'bg-purple-100 text-purple-700' },
+  { value: StatusOffer.ACCEPTED, color: 'bg-green-100 text-green-700' },
+  { value: StatusOffer.REJECTED, color: 'bg-red-100 text-red-700' },
+];
 
+export const StatusSelect = ({ value, onChange }: { value: string; onChange: (value: string) => void }) => {
   const { t } = useTranslation();
 
-  const statusLabels = {
-    sent: t('status.sent'),
-    followed_up: t('status.followed_up'),
-    interview_scheduled: t('status.interview_scheduled'),
-    accepted: t('status.accepted'),
-    rejected: t('status.rejected'),
-  };
+  const selectedOption = statusOptions.find(option => option.value === value);
 
   return (
     <Select value={value} onValueChange={onChange}>
       <SelectTrigger className='w-full'>
         <SelectValue>
-          <Badge className={cn('px-2 py-1 rounded-sm', statusColors[value])}>{statusLabels[value]}</Badge>
+          {selectedOption && (
+            <Badge className={cn('px-2 py-1 rounded-sm', selectedOption.color)}>
+              {t(`status.${selectedOption.value.toLowerCase().replace(' ', '_')}`)}
+            </Badge>
+          )}
         </SelectValue>
       </SelectTrigger>
       <SelectContent>
-        {Object.values(StatusOffer).map(status => (
-          <SelectItem key={status} value={status}>
-            <div className={cn('flex items-center gap-2')}>
-              <span className={cn('h-2 w-2 rounded-full', statusColors[status]?.replace('text-', 'bg-'))} />
-              {status}
-            </div>
+        {statusOptions.map(option => (
+          <SelectItem key={option.value} value={option.value}>
+            {t(`status.${option.value.toLowerCase().replace(' ', '_')}`)}
           </SelectItem>
         ))}
       </SelectContent>
--- client/src/router/routes.tsx
+++ client/src/router/routes.tsx
@@ -1,12 +1,13 @@
 import { Login } from '@/pages/Authentication/login';
 import { Register } from '@/pages/Authentication/register';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { ProtectedRoute } from '@/router/protectedRoute';
 
-function Home() {
-  return <h2>Accueil</h2>;
-}
+import { Button } from '@/components/ui/button';
+import { Plus } from 'lucide-react';
+import { t } from 'i18next';
+import { CreateJobApplication } from '@/pages/JobApplication/CreateJobApplication';
 
 function NotFound() {
   return <h1>Page not found</h1>;
@@ -39,7 +40,22 @@ export const Router = () => {
           path='/'
           element={
             <ProtectedRoute authRequired={true}>
-              <Home />
+              <h2>Accueil</h2>
+              <Link to='/create-application'>
+                <Button variant='outline' size='sm'>
+                  <Plus />
+                  {t('pages.home.button.add_job_application')}
+                </Button>
+              </Link>
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path='/create-application'
+          element={
+            <ProtectedRoute authRequired={true}>
+              <CreateJobApplication />
             </ProtectedRoute>
           }
         />
