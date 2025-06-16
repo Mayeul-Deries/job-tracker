1fde36db10e6dddde1d33c1a054fd9f3e3bc1f59
Mayeul Deries <mayeul.deries@gmail.com>
2025-06-16 18:25:28 +0200

    add little padding to the checkbox

--- client/src/pages/JobApplication/DataTable/columns.tsx
+++ client/src/pages/JobApplication/DataTable/columns.tsx
@@ -26,6 +26,7 @@ export const getColumns = (
     id: 'select',
     header: ({ table }) => (
       <Checkbox
+        className='mx-2'
         checked={table.getIsAllPageRowsSelected() || (table.getIsSomePageRowsSelected() && 'indeterminate')}
         onCheckedChange={value => table.toggleAllPageRowsSelected(!!value)}
         aria-label='Select all'
@@ -33,6 +34,7 @@ export const getColumns = (
     ),
     cell: ({ row }) => (
       <Checkbox
+        className='mx-2'
         checked={row.getIsSelected()}
         onCheckedChange={value => row.toggleSelected(!!value)}
         aria-label='Select row'
