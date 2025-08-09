625eaf543bdbdfb3a09efb1dd88b75954828d6d5
Mayeul Deries <mayeul.deries@gmail.com>
2025-08-09 23:41:17 +0200

    fix token expiration time

--- server/src/utils/constants/constants.js
+++ server/src/utils/constants/constants.js
@@ -1,6 +1,6 @@
 export const Constants = {
-  MAX_DURATION_COOKIE: 1000 * 60 * 60 * 24 * 30, // 30 days
-  MAX_DURATION_RESET_PASSWORD: 1000 * 60 * 15, // 15 minutes
+  MAX_DURATION_COOKIE: '30d', // 30 days
+  MAX_DURATION_RESET_PASSWORD: '15m', // 15 minutes
   USERNAME_REGEX: /^[a-zA-Z0-9._-]+$/,
   PASSWORD_REGEX: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&:.]).{8,}$/,
   EMAIL_REGEX: /\S+@\S+\.\S+/,
